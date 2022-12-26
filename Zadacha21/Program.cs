f395f334f5b91c0c1ee65d66dfb5d5232230dc01
Vitali_Pokrovski <vitaliipokrovskii91@gmail.com>
2022-12-27 00:17:09 +0500

    added Zadacha19 and Zadacha21

---
+++ Zadacha21/Program.cs
@@ -0,0 +1,23 @@
+﻿//  Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.Clear();
+Console.Write("Введите координату A: ");
+int xA = int.Parse(Console.ReadLine()!);
+Console.Write("Введите координату A: ");
+int yA = int.Parse(Console.ReadLine()!);
+Console.Write("Введите координату A: ");
+int zA = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите координату B: ");
+int xB = int.Parse(Console.ReadLine()!);
+Console.Write("Введите координату B: ");
+int yB = int.Parse(Console.ReadLine()!);
+Console.Write("Введите координату B ");
+int zB = int.Parse(Console.ReadLine()!);
+
+
+
+double result = Math.Sqrt((xA-xB)*(xA-xB) + (yA-yB)*(yA-yB) + (zA-zB)*(zA-zB));
+Console.WriteLine(result);
